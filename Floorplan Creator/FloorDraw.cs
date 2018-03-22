1c0015a5e6422d61512cac8b8f27e3107d89ddc9
LittleZeus <37666311+LittleZeus@users.noreply.github.com>
2018-03-22 16:25:42 +0000

    Add project files.

---
+++ Floorplan Creator/FloorDraw.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Floorplan_Creator
+{
+    class FloorDraw
+    {
+        static private Bitmap floor;
+        static private Pen pointPen = new Pen(Brushes.Blue);
+        static private Pen cyclePen = new Pen(Brushes.Orange);
+        static private Pen linePen = new Pen(Brushes.Green);
+
+        // creates an empty bitmap to the max size of the coordinates given.
+        static private void DrawFilledRectangle(int x, int y)
+        {
+            Bitmap bmp = new Bitmap(x, y);
+            using (Graphics graph = Graphics.FromImage(bmp))
+            {
+                Rectangle ImageSize = new Rectangle(0, 0, x, y);
+                graph.FillRectangle(Brushes.FloralWhite, ImageSize);               
+            }
+            floor = bmp;
+        }
+
+        // creates a area where a possible large room could be.
+        static private void CycleDraw(int[,] nodePos)
+        {
+
+            int arraySize = (nodePos.GetLength(0));
+            int X1, X2, Y1, Y2, lineX1, lineY1, lineX2, lineY2;
+            Queue cycleX = new Queue();
+            Queue cycleY = new Queue();
+            for (int i = 0; i < arraySize; i++)
+            {
+                X1 = nodePos[i, 0];
+                Y1 = nodePos[i, 1];
+                for (int j = i; j < arraySize; j++)
+                {
+                    X2 = nodePos[j, 0];
+                    Y2 = nodePos[j, 1];
+
+                    cycleX.Enqueue(X2);
+                    cycleY.Enqueue(Y2);
+
+                    if (X1 == X2 && Y1 == Y2)
+                    {
+                        // draw shape with queue of previous coords and empty queue.
+                        int queueSize = cycleX.Count - 1;
+                        for (int k = 0; k < queueSize; k++)
+                        {
+                            lineX1 = (int)cycleX.Dequeue();
+                            lineY1 = (int)cycleY.Dequeue();
+                            lineX2 = (int)cycleX.Peek();
+                            lineY2 = (int)cycleY.Peek();
+                            LineDraw(lineX1, lineY1, lineX2, lineY2, cyclePen);
+                        }
+                    }
+                }
+                cycleX.Clear();
+                cycleY.Clear();
+            }
+        }
+
+        //creates coloured pixels at the location specificied in the array.
+        static private void MarkMap(int[,] walkPos)
+        {
+            int xCoord;
+            int yCoord;
+            int arraySize = walkPos.GetLength(0);
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                xCoord = walkPos[i, 0];
+                yCoord = walkPos[i, 1];
+
+                LineDraw(xCoord-1, yCoord-1, xCoord+1, yCoord+1, pointPen);
+            }
+
+        }
+
+        //draws a line on the bitmap.
+        static private void LineDraw(int x1, int y1, int x2, int y2, Pen pen)
+        {       
+            using (Graphics graph = Graphics.FromImage(floor))
+            {
+                graph.DrawLine(pen, x1, y1, x2, y2);
+            }
+        }
+
+        //called to initiate the creation and drawing of the bitmap.
+        static public Bitmap CreateFloor(int[,] nodePos)
+        {
+            int arraySize = nodePos.GetLength(0);
+            linePen.Width = 1.0f;
+            cyclePen.Width = 4.0f;
+            pointPen.Width = 4.0f;
+
+            //finds the max x and y coordinates so the size of the bitmap can be determined.
+            int maxX = 1;
+            int maxY = 1;
+            for (int i = 0; i < arraySize; i++)
+            {
+                if (nodePos[i,0] > maxX)
+                {
+                    maxX = nodePos[i, 0];
+                }
+                if (nodePos[i, 1] > maxY)
+                {
+                    maxY = nodePos[i, 1];
+                }
+            }
+
+            DrawFilledRectangle(maxX+50, maxY+50);
+
+            //draws highlights for the cycles
+            CycleDraw(nodePos);
+
+            //inputs the coordinates for creating the lines.
+            for (int i = 0; i <= arraySize-2; i++)
+            {
+                int x1 = nodePos[i, 0];
+                int y1 = nodePos[i, 1];
+                int x2 = nodePos[i + 1, 0];
+                int y2 = nodePos[i + 1, 1];
+                LineDraw(x1, y1, x2, y2, linePen);
+            }
+
+            MarkMap(nodePos);
+
+            linePen.Dispose();
+            return floor;          
+        }
+
+    }
+}
---
+++ Floorplan Creator/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace Floorplan_Creator
+{
+    class Program
+    {
+        // removes repeats that occur straight after one another.
+        static public int[,] RemoveRepeats(int[,] coords)
+        {
+            int[,] returnCoords;
+            int X1, X2, Y1, Y2;
+            int arraySize = (coords.GetLength(0)-1);
+            Queue coordX = new Queue();
+            Queue coordY = new Queue();
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                X1 = coords[i, 0];
+                Y1 = coords[i, 1];
+                X2 = coords[i+1, 0];
+                Y2 = coords[i+1, 1];
+                if (X1 != X2 && Y1 != Y2)
+                {
+                    coordX.Enqueue(X1);
+                    coordY.Enqueue(Y1);
+                }
+                if (i < arraySize+1)
+                {
+                    coordX.Enqueue(X2);
+                    coordY.Enqueue(Y2);
+                }
+            }
+
+            int queueSize = coordX.Count;
+            returnCoords = new int[queueSize, 2];
+            for (int i = 0; i < queueSize; i++)
+            {
+                returnCoords[i, 0] = (int)coordX.Dequeue();
+                returnCoords[i, 1] = (int)coordY.Dequeue();
+            }
+
+            return returnCoords;
+        }
+
+            //rounds coordinates together if they are close enough.
+        static public int[,] RoundCoords(int[,] coords)
+        {
+            int arraySize = (coords.GetLength(0));
+            int X1, X2, Y1, Y2;
+            int roundTo = 100;
+            for (int i = 0; i < arraySize; i++)
+            {
+                X1 = coords[i, 0];
+                Y1 = coords[i, 1];
+                for (int j = 0; j < arraySize; j++)
+                {
+                    X2 = coords[j, 0];
+                    Y2 = coords[j, 1];
+                    if ((X1- roundTo <= X2) && (X1+ roundTo >= X2) && (Y1 - roundTo <= Y2) && (Y1 + roundTo >= Y2) )
+                    {
+                        coords[j, 0] = X1;
+                        coords[j, 1] = Y1;
+                    }
+                }
+            }
+                return coords;
+        }
+
+        // trys to round the path of the coords.
+        // finds the mid point of (x1,y1) and (x3,y3) which is (xm,ym), then 25% of (xm,ym) and 75% of (x2,y2).
+        // then writes the new coord to (x2,y2) in the array. It goes through the entire array doing this.
+        static public int[,] SmoothPath (int[,] coords)
+        {
+            int arraySize = (coords.GetLength(0))-2;
+            double X1, X2, X3, Xm, Xm2, Xm3, Y1, Y2, Y3, Ym, Ym2, Ym3;
+            for (int i = 0; i < arraySize; i++)
+            {
+                X1 = coords[i, 0];
+                Y1 = coords[i, 1];
+                X2 = coords[i + 1, 0];
+                Y2 = coords[i + 1, 1];
+                X3 = coords[i + 2, 0];
+                Y3 = coords[i + 2, 1];
+                Xm = (X1 + X3) / 2;
+                Ym = (Y1 + Y3) / 2;
+                Xm2 = (X2 + Xm) / 2;
+                Ym2 = (Y2 + Ym) / 2;
+                Xm3 = (X2 + Xm2) / 2;
+                Ym3 = (Y2 + Ym2) / 2;
+                coords[i + 1, 0] = (int)Xm3;
+                coords[i + 1, 1] = (int)Ym3;
+            }
+
+            return coords;
+        }
+
+        //read the file containing coordinates and put them into an array.
+        static public int[,] ReadCoords(String fileRead)
+        {
+            int[,] walkCoords;
+            string[] coords;
+            int counter = 0;
+            string line;
+            // read the file to find amount of lines so array can be sized correctly.
+            System.IO.StreamReader file = new System.IO.StreamReader(fileRead);
+            while ((line = file.ReadLine()) != null)
+            {
+                counter++;
+            }
+            file.Close();
+
+            walkCoords = new int[counter, 2];
+            coords = new string[counter];
+            counter = 0;
+
+            //reads the file each line input into the array. (Xcoord, Ycoord)
+            file = new System.IO.StreamReader(fileRead);
+            while ((line = file.ReadLine()) != null)
+            {
+                
+                //Debug.WriteLine(line);
+                coords[counter] = line;
+                counter++;
+            }
+            file.Close();
+
+            //takes the string array then splits and converts to interger.
+            int arraySize = coords.Length;
+            for (int i = 0; i < arraySize; i++)
+            {
+                string pos = coords[i];
+
+                var res = pos.Split(',').ToArray();
+                walkCoords[i, 0] = Int32.Parse(res[0]);
+                walkCoords[i, 1] = Int32.Parse(res[1]);
+            }
+
+                return walkCoords;
+        }
+
+        //creates a array for testing
+        static public int[,] PopulateArray(int lenght)
+        {
+            int[,] walkCoords = new int[lenght, 2];
+            int arraySize = walkCoords.GetLength(0);
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                walkCoords[i, 0] = i+1;
+                walkCoords[i, 1] = i+1;
+            }
+
+            return walkCoords;
+        }
+
+        static void Main()
+        {
+            string file = "C:/Users/Ben/Documents/Visual Studio 2015/Projects/Floorplan Creator/Floorplan Creator/bin/Debug/CoordData.txt";
+            int[,] walkCoords = RemoveRepeats(RoundCoords(SmoothPath(ReadCoords(file))));
+            //Debug.WriteLine(walkCoords[1,0]);
+            Bitmap floorplan = FloorDraw.CreateFloor(walkCoords);
+            //saves the bitmap to file.jpeg
+            floorplan.Save("file.jpeg", ImageFormat.Png);
+        }
+    }
+}
+
